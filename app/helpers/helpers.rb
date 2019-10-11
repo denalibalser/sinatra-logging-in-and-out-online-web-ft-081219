c12b4bc93cd708e59b79c06328f718e133be5faa
Denali Balser <denali.balser167@gmail.com>
2019-10-11 21:15:36 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find_by(user_id: params[:user_id])
+    if @user
+      session[:user_id] = @user.id
+      @user
+    end
+  end
+
+  def self.is_logged_in?(session)
+    session[:user_id] != nil
+    
+  end
 end
